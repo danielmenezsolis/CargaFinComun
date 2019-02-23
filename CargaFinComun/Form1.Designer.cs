a93e8a904e7fac66c6133f6aab31ce38e6fa55ab
Daniel Menez Solis <daniel_menez@hotmail.com>
2019-02-23 16:53:46 -0600

    Agregar archivos de proyecto.

---
+++ CargaFinComun/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace CargaFinComun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblFile = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.PBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRecords = new System.Windows.Forms.Label();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblFile
+            // 
+            this.lblFile.AutoSize = true;
+            this.lblFile.Location = new System.Drawing.Point(151, 50);
+            this.lblFile.Name = "lblFile";
+            this.lblFile.Size = new System.Drawing.Size(37, 13);
+            this.lblFile.TabIndex = 1;
+            this.lblFile.Text = "No file";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(329, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 21);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load Records";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PBar
+            // 
+            this.PBar.Location = new System.Drawing.Point(12, 113);
+            this.PBar.Name = "PBar";
+            this.PBar.Size = new System.Drawing.Size(458, 23);
+            this.PBar.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Records:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "File Path:";
+            // 
+            // lblRecords
+            // 
+            this.lblRecords.AutoSize = true;
+            this.lblRecords.Location = new System.Drawing.Point(151, 84);
+            this.lblRecords.Name = "lblRecords";
+            this.lblRecords.Size = new System.Drawing.Size(50, 13);
+            this.lblRecords.TabIndex = 6;
+            this.lblRecords.Text = "Records:";
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 142);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(458, 150);
+            this.dataGrid.TabIndex = 7;
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(476, 6);
+            this.txtBody.MaxLength = 1000000;
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBody.Size = new System.Drawing.Size(407, 286);
+            this.txtBody.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 304);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.lblRecords);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.PBar);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblFile);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Carga";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblFile;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar PBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRecords;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.TextBox txtBody;
+    }
+}
+
---
+++ CargaFinComun/Form1.cs
@@ -0,0 +1,202 @@
+﻿using CsvHelper;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CargaFinComun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "CSV files|*.csv";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = theDialog.FileName;
+                string[] filelines = File.ReadAllLines(filename);
+                lblFile.Text = filename;
+                lblRecords.Text = filelines.Count().ToString();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string[] lines = File.ReadAllLines(lblFile.Text, Encoding.UTF8);
+            CsvReader csv = new CsvReader(File.OpenText(lblFile.Text));
+            csv.Read();
+            csv.ReadHeader();
+
+            List<string> headers = csv.Context.HeaderRecord.ToList();
+
+            // Read csv into datatable
+            DataTable dataTable = new DataTable();
+
+            foreach (string header in headers)
+            {
+                dataTable.Columns.Add(new System.Data.DataColumn(header));
+            }
+
+            // Check all required columns are present
+            if (!headers.Exists(x => x == "idSucursal"))
+            {
+                throw new ArgumentException("idSucursal field not present in input file.");
+            }
+            else if (!headers.Exists(x => x == "Prioridad"))
+            {
+                throw new ArgumentException("Prioridad field not present in input file.");
+            }
+            else if (!headers.Exists(x => x == "nombre"))
+            {
+                throw new ArgumentException("nombre field not present in input file.");
+            }
+
+            while (csv.Read())
+            {
+                System.Data.DataRow row = dataTable.NewRow();
+
+                foreach (System.Data.DataColumn column in dataTable.Columns)
+                {
+                    row[column.ColumnName] = csv.GetField(column.DataType, column.ColumnName);
+                }
+
+                dataTable.Rows.Add(row);
+            }
+
+            if (dataTable != null)
+            {
+                dataGrid.DataSource = dataTable;
+                if (dataTable.Rows.Count != 0)
+                {
+                    InsertActivities(dataTable);
+                }
+            }
+
+
+        }
+
+        private void InsertActivities(DataTable dataTable)
+        {
+
+            PBar.Minimum = 1;
+            PBar.Maximum = 1000;
+            PBar.Value = 1;
+            // Set the Step property to a value of 1 to represent each file being copied.
+            PBar.Step = 1;
+            string body = "{ \n";
+            body += "\"updateParameters\" : { \n ";
+
+            body += "\"identifyActivityBy\" : \"apptNumber\" , \n";
+            body += "\"ifInFinalStatusThen\" : \"doNothing\" \n ";
+            body += "}, \n ";
+            body += "\"activities\" : [ \n ";
+
+            int i = 0;
+            foreach (DataRow row in dataTable.Rows)
+            {
+                i++;
+
+                body += "{ \n";
+                body += "\"timeSlot\": \"Todo el d\u00eda \", \n";
+                body += "\"resourceId\":34, \n";
+
+                //TipoActividad,id,idK,idSucursal,Prioridad,apellidoPaterno,apellidoMaterno,nombre,fechaNacimiento,sexo,Domicilio,lat,lng,telefonoFijo,telefonoCelular,capitalInicial,,interesesOrdinariosPendiente,interesesMoratoriosPendiente,iva,totalPendiente,numeroPagosPendientes,numeroPagosRealizados,montoPagoFrecuente,fechaUltimoPago,diasMoratorios,importeUltimoPago,fechaCreditoOtorgamiento,saldoExigible,date
+                body += "\"activityType\":\"" + row["TipoActividad"].ToString() + "\", \n";
+                body += "\"apptNumber\":\"" + row["id"].ToString() + "\", \n";
+                body += "\"capitalInicial\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"capitalPendiente\":\"" + row["capitalPendiente"].ToString() + "\", \n";
+                body += "\"interesesOrdinariosPendiente\":\"" + row["interesesOrdinariosPendiente"].ToString() + "\", \n";
+                body += "\"interesesMoratoriosPendiente\":\"" + row["interesesMoratoriosPendiente"].ToString() + "\", \n";
+                body += "\"iva\":\"" + row["iva"].ToString() + "\", \n";
+                body += "\"totalPendiente\":\"" + row["totalPendiente"].ToString() + "\", \n";
+                body += "\"capitalAbonado\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"interesesOrdinariosAbonado\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"interesesMoratoriosAbonado\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"totalAbonado\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"numeroPagosPendientes\":\"" + row["numeroPagosPendientes"].ToString() + "\", \n";
+                body += "\"numeroPagosRealizados\":\"" + row["numeroPagosRealizados"].ToString() + "\", \n";
+                body += "\"montoPagoFrecuente\":\"" + row["montoPagoFrecuente"].ToString() + "\", \n";
+                body += "\"fechaUltimoPago\":\"" + row["fechaUltimoPago"].ToString() + "\", \n";
+                body += "\"diasMoratorios\":\"" + row["diasMoratorios"].ToString() + "\", \n";
+                body += "\"importeUltimoPago\":\"" + row["importeUltimoPago"].ToString() + "\", \n";
+                body += "\"importeUltimoPago\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"saldoExigible\":\"" + row["saldoExigible"].ToString() + "\", \n";
+                body += "\"aPaterno\":\"" + row["apellidoPaterno"].ToString() + "\", \n";
+                body += "\"aMaterno\":\"" + row["apellidoMaterno"].ToString() + "\", \n";
+                body += "\"idSucursal\":\"" + row["idSucursal"].ToString() + "\", \n";
+                body += "\"fechaNacimiento\":\"" + row["fechaNacimiento"].ToString() + "\", \n";
+                body += "\"customerName\":\"" + row["nombre"].ToString() + "\", \n";
+                body += "\"customerPhone\":\"" + row["telefonoCelular"].ToString() + "\", \n";
+                body += "\"customerNumber\":\"" + row["telefonoFijo"].ToString() + "\", \n";
+                body += "\"Prioridad\":\"" + row["Prioridad"].ToString() + "\", \n";
+                body += "\"deliveryWindowStart\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"deliveryWindowEnd\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"startTime\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"endTime\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"streetAddress\":\"" + row["Domicilio"].ToString() + "\", \n";
+                body += "\"numeroInt\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"date\":\"" + row["date"].ToString() + "\", \n";
+                body += "\"city\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"estado\":\"" + row["capitalInicial"].ToString() + "\", \n";
+                body += "\"postalCode\":\"" + row["capitalInicial"].ToString() + "\" \n";
+
+                if (i == 1000)
+                {
+                    body += "} \n ";
+                    break;
+                }
+                else
+                {
+                    body += "},\n ";
+                }
+                PBar.PerformStep();
+            }
+            body += "] \n";
+            body += "} \n";
+            txtBody.Text = body;
+            dataGrid.DataSource = null;
+            dataGrid.DataSource = dataTable;
+
+
+            /*
+                     var client = new RestClient("https://fincomunfieldserv1.test.etadirect.com/");
+
+                     client.Authenticator = new HttpBasicAuthenticator("mid@fincomunfieldserv1.test", "3a2aeb2ced436bcdddd8791bba5b279801076e04c51cf7243807d6563607e936");
+                     var request = new RestRequest("rest/ofscCore/v1/activities/custom-actions/bulkUpdate", Method.POST)
+                     {
+                         RequestFormat = DataFormat.Json,
+                     };
+                     request.AddParameter("application/json", body, ParameterType.RequestBody);
+                     IRestResponse response = client.Execute(request);
+                     var content = response.Content;
+                     if (response.StatusCode == HttpStatusCode.Created)
+                     {
+                     }
+                     else
+                     {
+                         MessageBox.Show("Error" + content);
+                     }
+                     */
+        }
+    }
+
+}
+
+
